d58e69a9ce31cf6b44e2ca0c65081a155ba03811
avik-ch <181900631+avik-ch@users.noreply.github.com>
2025-08-08 14:38:35 -0500

    some minor changes

--- server/app.js
+++ server/app.js
@@ -1,15 +1,36 @@
 require("dotenv").config();
 const express = require("express");
-const cors = reuqire("cors");
+const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
-app.use(cors());
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5713",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
 // add routes
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
